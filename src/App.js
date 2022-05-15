3d609d1c34e497cd6694c54cc500e8605926af9a
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-05-16 01:08:32 +0530

    connected to the firebase and added basic logic for google auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
+import { useEffect, useState } from "react";
+import { provider, db, auth } from "./config/firebase";
+import { signInWithPopup,signOut} from 'firebase/auth';
+
 function App() {
+  const [user, setUser] = useState(() => auth.currentUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      console.log(user);
+    });
+  }, []);
+
+  const signInWithGoogle = async () => {
+    try {
+      provider.setCustomParameters({ prompt: 'select_account' });
+      await signInWithPopup(auth,provider);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logOut = async () => {
+    signOut(auth)
+    .then(() => {
+        console.log('logged out');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+  };
+
   return (
-   <div>
-     <h1>
-       home
-     </h1>
-   </div>
+    <div className="container">
+    <main>
+      {user ? (
+        <>
+          <nav id="sign_out">
+            <h2>Chat With Friends</h2>
+            <button onClick={logOut}>Sign Out</button>
+          </nav>
+          {/* <ChatRoom user={user} db={db} /> */}
+        </>
+      ) : (
+        <section id="sign_in">
+          <h1>Welcome to Chat Room</h1>
+          <button onClick={signInWithGoogle}>Sign In With Google</button>
+        </section>
+      )}
+    </main>
+  </div>
   );
 }
 
 export default App;
+
